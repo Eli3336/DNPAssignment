828101a4b8633f4c341c5bdc0a39d57176106d2d
Xbey25 <luisdgb2506@gmail.com>
2022-11-01 15:19:41 +0100

    Update User.cs

--- DNPAssignment/Domain/Models/User.cs
+++ DNPAssignment/Domain/Models/User.cs
@@ -5,4 +5,13 @@ public class User
     public int Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
+
+
+    public User(int iD, string userName, string password)
+    {
+        Id = iD;
+        UserName = userName;
+        Password = password;
+
+    }
 }

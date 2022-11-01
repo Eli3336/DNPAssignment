96a9d74167bc4e3b8d0413b019f2adf75518976f
Xbey25 <luisdgb2506@gmail.com>
2022-11-01 09:26:37 +0100

    Small changes to program to create user

--- DNPAssignment/WebAPI/Program.cs
+++ DNPAssignment/WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using Domain.DaoInterfaces;
+using Domain.Logic;
+using Domain.LogicInterfaces;
+using FileData;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<FileContext>();
+builder.Services.AddScoped<IUserDao, UserFileDAO>();
+builder.Services.AddScoped<IUserLogic, UserLogic>();
 
 var app = builder.Build();
 

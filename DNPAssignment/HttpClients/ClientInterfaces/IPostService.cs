a76cd1c2d1db4a36922ab8bc74e5fc134b50377b
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2022-11-09 13:05:35 +0100

    http

---
+++ DNPAssignment/HttpClients/ClientInterfaces/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IPostService
+{
+    Task<ICollection<Post>> GetAsync(
+        string? userName,
+        string? userID,
+        int? id, 
+        string? titleContains, 
+        string? body
+    );
+    
+}
---
+++ DNPAssignment/HttpClients/Implementations/PostHttpClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using Domain.Models;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.Implementations;
+
+public class PostHttpClient : IPostService
+{
+    private readonly HttpClient client;
+
+    public PostHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task<ICollection<Post>> GetAsync(string? userName, string? userID, int? id, string? titleContains, string? body)
+    {
+        HttpResponseMessage response = await client.GetAsync("/Posts");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        ICollection<Post> posts = JsonSerializer.Deserialize<ICollection<Post>>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return posts;
+    }
+}
